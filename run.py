d7395cc6823f2b4b225a3a85a666c3d3fb0fa87e
AshFoster <ashfoster87@gmail.com>
2021-09-22 15:39:48 +0000

    Print out the calculated stock numbers for each sandwich type

--- run.py
+++ run.py
@@ -115,6 +115,17 @@ def calculate_stock_data(data):
     return new_stock_data
 
 
+def get_stock_values(data):
+    """
+    Print out the calculated stock numbers for each sandwich type.
+    """
+    headings = SHEET.worksheet("stock").row_values(1)
+    
+    print("Make the following numbers of sandwiches for next market:\n")
+    
+    return {heading: data for heading, data in zip(headings, data)}
+
+
 def main():
     """
     Run all program functions
@@ -127,6 +138,8 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    stock_values = get_stock_values(stock_data)
+    print(stock_values)
 
 
 print("Welcome to Love Sandwiches Data Automation")
